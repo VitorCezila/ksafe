319f79364d07fcb075adb035eb2c8a381b01f6d3
Cezila <vcezila@gmail.com>
2023-10-05 10:24:30 -0300

    feat: Implemented views of screen

---
+++ app/src/main/java/com/cezila/passwordmanager/ui/password_detail_screen/PasswordDetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.cezila.passwordmanager.ui.password_detail_screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PasswordDetailViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
